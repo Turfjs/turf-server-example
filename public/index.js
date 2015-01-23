082d1f61d6883be1cecfa461f3c752c82fe2de7b
Tom MacWright <tom@macwright.org>
2015-01-23 11:57:12 -0800

    Fix base path

--- public/index.js
+++ public/index.js
@@ -5,9 +5,11 @@ if (window.location.search) {
     overlay.parentNode.removeChild(overlay);
 }
 
+var basePath = window.location.href.split('?')[0].split('#')[0];
+
 // Add the user to the map and get their current
 // location
-request(location.href + 'add', { json: true }, function(err, res, body) {
+request(basePath + 'add', { json: true }, function(err, res, body) {
     L.mapbox.accessToken = 'pk.eyJ1IjoidG1jdyIsImEiOiJIZmRUQjRBIn0.lRARalfaGHnPdRcc-7QZYQ';
     // Create a map in the div #map
     var map = L.mapbox.map('map', 'tmcw.l12c66f2', {
@@ -32,7 +34,7 @@ request(location.href + 'add', { json: true }, function(err, res, body) {
     map.fitBounds(ownpoint.getBounds());
 
     // get the triangulated version of the current data
-    request(location.href + 'tin', { json: true }, function(err, res, body) {
+    request(basePath + 'tin', { json: true }, function(err, res, body) {
         L.geoJson(body, {
             style: function(feature) {
                 return {
@@ -47,7 +49,7 @@ request(location.href + 'add', { json: true }, function(err, res, body) {
     });
 
     // get the raw data
-    request(location.href + 'points', { json: true }, function(err, res, body) {
+    request(basePath + 'points', { json: true }, function(err, res, body) {
         L.mapbox.featureLayer(body).addTo(map).eachLayer(function(l) {
             l.bindPopup('<h2>' + l.feature.properties.speed.toFixed(2) + ' kb/s</h2>');
         });
